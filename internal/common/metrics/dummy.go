93a60cc1d5b7f3da447ecf29c5bf3d506b279093
maixuanbach174 <22125005@student.hcmus.edu.vn>
2025-10-26 13:48:19 +0700

    Set up integration test and unit test

--- internal/common/metrics/dummy.go
+++ internal/common/metrics/dummy.go
@@ -1,10 +1,7 @@
 package metrics
 
-import "github.com/sirupsen/logrus"
-
 type NoOp struct{}
 
 func (d NoOp) Inc(_ string, _ int) {
 	// todo - add some implementation!
-	logrus.Info("NoOp metrics client")
 }
