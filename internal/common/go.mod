ddd9fad593cb0919fb6683f6e42ded1d70c74ea3
maixuanbach174 <22125005@student.hcmus.edu.vn>
2025-10-27 14:23:31 +0700

    Finish setting up web server

--- internal/common/go.mod
+++ internal/common/go.mod
@@ -4,6 +4,7 @@ go 1.25.2
 
 require (
 	github.com/go-chi/chi/v5 v5.2.3
+	github.com/go-chi/cors v1.2.2
 	github.com/go-chi/render v1.0.3
 	github.com/oapi-codegen/runtime v1.1.2
 	github.com/sirupsen/logrus v1.9.3
--- internal/education/main.go
+++ internal/education/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"context"
+	"net/http"
 
+	"os"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
 	"github.com/maixuanbach174/online-course-app/internal/common/logs"
+	"github.com/maixuanbach174/online-course-app/internal/common/server"
+	"github.com/maixuanbach174/online-course-app/internal/education/ports"
 	"github.com/maixuanbach174/online-course-app/internal/education/services"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -14,8 +22,19 @@ func main() {
 
 	application, err := services.NewApplication(ctx)
 	if err != nil {
-		return
+		logrus.WithError(err).Fatal("Failed to initialize application")
 	}
 
 	defer application.Close()
+
+	serverType := strings.ToLower(os.Getenv("SERVER_TO_RUN"))
+	switch serverType {
+	case "http":
+		server.RunHTTPServer(func(router chi.Router) http.Handler {
+			return ports.HandlerFromMux(
+				ports.NewHttpServer(application.App),
+				router,
+			)
+		})
+	}
 }
